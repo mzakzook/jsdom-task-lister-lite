d22d05689b75f3c5d51ee1389ce0a248a225ddd2
Max Zakzook <mzakzook@me.com>
2019-10-29 14:22:17 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', function(event) {
+  addComments();
 });
+
+
+function addComments() {
+  const commentForm = document.getElementById('create-task-form');
+
+  commentForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    // const submittedForm = event.target
+    // const inputField = submittedForm.getElementById('new-comment')
+
+    const userInputField = event.target.querySelector('#new-task-description');
+
+    const commentsContainer = document.querySelector('#tasks');
+    const newComment = document.createElement('li');
+    newComment.textContent = userInputField.value;
+    commentsContainer.appendChild(newComment);
+
+    userInputField.value = '';
+    userInputField.focus();
+
+    // window.location.href = 'about.html';
+
+    // commentsContainer.innerHTML += `<p>${userInputField.value}</p>`
+  });
+}
